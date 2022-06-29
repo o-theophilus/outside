370a40f89cf3cfdafa890e50aaebe1a5e1b0e15c
theophilus <theophilus.ogbolu@gmail.com>
2022-06-29 09:37:38 +0100

    frontend init

---
+++ frontend/src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				status,
+				error
+			}
+		};
+	}
+</script>
+
+<script>
+
+
+	export let status;
+	export let error;
+</script>
+
+<svelte:head>
+	<title>{status} Error | Meji</title>
+</svelte:head>
+
+{@html error.message}
+{status}
---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+
+
+<script>
+
+
+
+
+
+	let innerWidth;
+	let scrollY;
+</script>
+
+<svelte:window bind:innerWidth bind:scrollY />
+
+<slot />
+
+<style>
+</style>
