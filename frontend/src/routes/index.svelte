238bce14f7d9b2021e0acb178ea8f3494cbfc8c8
theophilus <theophilus.ogbolu@gmail.com>
2022-06-29 11:49:37 +0100

    fiz 1

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	import { backend } from '$lib/store.js';
 
-	import Item from '$lib/Item.svelte';
+	// import Item from '$lib/Item.svelte';
 
 	export async function load({ fetch }) {
 		const _resp = await fetch(`${backend}/nft`, {
@@ -29,6 +29,6 @@
 	let length = 100;
 </script>
 
-{#each meta.slice(0, length) as m}
+<!-- {#each meta.slice(0, length) as m}
 	<Item meta={m} />
-{/each}
+{/each} -->
